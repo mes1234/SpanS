5932bba40f714cdb9085e4c56a2d3c5090f0688a
WK WK <witold.kepczynski@gmail.com>
2022-08-13 10:01:01 +0200

    Add project files.

---
+++ SpanS/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace SpanS
+{
+    public static class MagicMaker
+    {
+        public static string StringNormalMagic(string input)
+        {
+            return string.Empty;
+        }
+
+
+        public static ReadOnlySpan<char> StringSpanMagic(ReadOnlySpan<char> input)
+        {
+            return new ReadOnlySpan<char>(input.ToArray());
+        }
+    }
+}
