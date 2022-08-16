339e8101f54d3e7d1af5d4b213680c0dac426767
WK WK <witold.kepczynski@gmail.com>
2022-08-16 23:09:25 +0200

    some data parsing

---
+++ Playground/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpanS;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+
+var data = await MeteoClient.GetMeteo();
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+var parsedData = JsonSerializer.Deserialize<MeteoDataDto>(data, options);
+
+Console.WriteLine(parsedData!.ToString());
---
+++ SpanS/MeteoDataDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpanS
+{
+    public class MeteoDataDto
+    {
+        public double Latitude { get; init; }
+        public double Longitude { get; init; }
+        public double Generationtime_ms { get; init; }
+        public int Utc_offset_seconds { get; init; }
+        public string? Timezone { get; init; }
+        public string? Timezone_abbreviation { get; init; }
+        public double Elevation { get; init; }
+        public UnitDto? Hourly_units { get; init; }
+        public DataDto? Hourly { get; init; }
+    }
+
+    public class UnitDto
+    {
+        public string? Time { get; set; }
+        public string? Temperature_2m { get; set; }
+        public string? Relativehumidity_2m { get; set; }
+        public string? Windspeed_10m { get; set; }
+    }
+
+    public class DataDto
+    {
+        public IEnumerable<DateTime> Time { get; init; } = Enumerable.Empty<DateTime>();
+        public IEnumerable<double> Temperature_2m { get; init; } = Enumerable.Empty<double>();
+        public IEnumerable<double> Relativehumidity_2m { get; init; } = Enumerable.Empty<double>();
+        public IEnumerable<double> Windspeed_10m { get; init; } = Enumerable.Empty<double>();
+    }
+}
